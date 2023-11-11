f90f67206ab74679db36c7eaf41d5f476c3e0570
= <brazhnik-03@list.ru>
2023-11-11 21:37:29 +0700

    docs: Написал документацию

--- IntroToIT/lesson2/Task2/Task.py
+++ IntroToIT/lesson2/Task2/Task.py
@@ -1,6 +1,6 @@
 #INTRO TO IT 2nd COURSE
 # Задача 2: Переверни строку!
 # Ученик, твоя задача перевернуть строку, используя срезы в Python.
-def перевернуть_строку(строка):
-    return строка[::-1]
-print(перевернуть_строку("Python"))
+def invert_string(string):
+    return string[::-1]
+print(invert_string("Python"))
--- IntroToIT/lesson2/Task9/Task.py
+++ IntroToIT/lesson2/Task9/Task.py
@@ -1,7 +1,7 @@
 #INTRO TO IT 2nd COURSE
 # Задача 9: Палиндром ли это?
 # Определи, является ли введенная строка палиндромом.
-def это_палиндром(строка):
-    return строка == строка[::-1]
-строка = "радар"
-print(f"Является ли '{строка}' палиндромом? {это_палиндром(строка)}")
+def his_is_a_palindrome(line):
+    return line == line[::-1]
+line = "radar"
+print(f"Is_li '{line}' palindrome? {his_is_a_palindrome(line)}")
